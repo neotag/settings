3e805be32dd01afa5537b452659d4b25a81c7458
neotag <harada@gaji.jp>
2016-12-18 15:16:42 +0900

    Add 'Defaults' settings

--- setup.sh
+++ setup.sh
@@ -1,2 +1,16 @@
+# Defaults
+defaults write com.apple.dock autohide -int 1
+defaults write com.apple.dock magnification -int 1
+killall Dock
+defaults write com.apple.finder AppleShowAllFiles YES
+killall Finder
+# Can`t setting key repeat
+# https://github.com/mathiasbynens/dotfiles/issues/687
+mkdir -p ~/Pictures/Screenshots/
+defaults write com.apple.screencapture location ~/Pictures/Screenshots/
+defaults write com.apple.screencapture name "screenshot_"
+killall SystemUIServer
+
+
 cd ~/ && ln -s gh/settings/dotfiles/.* ./ && cd -
 
