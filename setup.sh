e93a8d5b240f4d4548ef3cfda9933ffea017fdf6
neotag <harada@gaji.jp>
2016-12-18 15:18:46 +0900

    Install anyenv, rbenv and ndenv

--- setup.sh
+++ setup.sh
@@ -14,3 +14,26 @@ killall SystemUIServer
 
 cd ~/ && ln -s gh/settings/dotfiles/.* ./ && cd -
 
+# Tools
+
+## Vagrant
+
+## Sierra で vagrant の curl が動かないので削除して system のをつかう
+sudo mv /opt/vagrant/embedded/bin/curl /opt/vagrant/embedded/bin/curl_renamed_for_sierra
+vagrant plugin install vagrant-vbguest
+vagrant plugin install vagrant-vmware-fusion
+vagrant plugin license vagrant-vmware-fusion ~/Dropbox/license/vagrant-vmware-provider/license.lic
+
+## Anyenv
+
+git clone https://github.com/riywo/anyenv ~/.anyenv
+echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(anyenv init -)"' >> ~/.bash_profile
+echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.zprofile
+echo 'eval "$(anyenv init -)"' >> ~/.zprofile
+exec $SHELL -l
+
+anyenv install rbenv
+anyenv install ndenv
+exec $SHELL -l
+
